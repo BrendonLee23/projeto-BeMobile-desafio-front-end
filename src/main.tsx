f72ca8e4263b250af73566f9373cec953f3f964a
Brendo <brendow_prime@hotmail.com>
2024-02-25 17:18:52 -0400

    Feat: Folder organization + HomePage creation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App.tsx';
+import ResetStyle from './assets/styles/ResetStyle.tsx';
+import GlobalStyle from './assets/styles/GobalStyle.tsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ResetStyle/>
+    <GlobalStyle/>
     <App />
   </React.StrictMode>,
 )
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components"
+
+export default function HomePage(){
+    return(
+        <>
+        <Text>Home Page </Text>
+        </>
+    )
+}
+
+const Text = styled.h1`
+    font-family: 'Roboto', sans-serif;
+`
