2d9f75e1134f0e3ad378e848e80170b980341fd4
Brendo <brendow_prime@hotmail.com>
2024-02-26 19:53:04 -0400

    Feat: input structure functions

--- src/components/Forms/Input.tsx
+++ src/components/Forms/Input.tsx
@@ -1,12 +1,37 @@
+import { SetStateAction, useState } from "react";
 import searchIcon from "../../assets/images/lupa.svg";
 import StyledInput from "./Input-Styles";
 
 export default function InputArea() {
+    const [searchTerm, setSearchTerm] = useState("");
+    console.log(searchTerm)
+
+    const inputChangeFunction = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const searchFunction = () => {
+        // Adicinar Lógica de Busca pelos dados na API
+        console.log("Pesquisando por:", searchTerm);
+    };
+
+    const searchKeyDownFunction = (event: { key: string; }) => {
+        if (event.key === "Enter") {
+            searchFunction();
+        }
+    };
+
     return (
         <StyledInput>
-            <input type="text" placeholder="Pesquisar" />
-            <img src={searchIcon} alt="searchIcon" />
+            <input 
+                type="text" 
+                placeholder="Pesquisar" 
+                value={searchTerm} 
+                onChange={inputChangeFunction} 
+                onKeyDown={searchKeyDownFunction} 
+                autoComplete="on"
+            />
+            <img src={searchIcon} alt="searchIcon" onClick={searchFunction} /> 
         </StyledInput>
-    )
+    );
 }
-
