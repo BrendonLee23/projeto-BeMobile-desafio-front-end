2ac915be9154b20ee1b21c3d4a2aa19aeeef94ea
Brendo <brendow_prime@hotmail.com>
2024-03-01 14:58:44 -0400

    Fix: formatNumber function fixed error

--- src/utils/formatPhoneNumber.tsx
+++ src/utils/formatPhoneNumber.tsx
@@ -1,14 +1,18 @@
 const formatPhoneNumber = (phoneNumber: string) => {
     const cleaned = phoneNumber.replace(/\D/g, '');
     
-    const countryCode = cleaned.startsWith('55') ? '+55' : '';
+    let countryCode = '';
+    if (cleaned.startsWith('55')) {
+        countryCode = '+55';
+    }
     
     const match = cleaned.match(/^(\d{2})(\d{2})(\d{5})(\d{4})$/);
     if (match) {
-        return `${countryCode} (${match[1]}) ${match[2]}-${match[3]}${match[4]}`;
+        return `${countryCode} (${match[2]}) ${match[3]}-${match[4]}`;
     } else {
         return phoneNumber;
     }
 };
 
 export default formatPhoneNumber;
+
